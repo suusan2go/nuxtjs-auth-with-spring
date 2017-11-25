18a8fde3179506d46773b91cd571f68c9df9289f
suusan2go <ksuzuki180@gmail.com>
2017-11-25 22:15:24 +0900

    change to typescript template

--- nuxt-front/nuxt.config.js
+++ nuxt-front/nuxt.config.js
@@ -21,18 +21,6 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, ctx) {
-      if (ctx.dev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+  },
+  modules: ['~/modules/typescript']
 }
