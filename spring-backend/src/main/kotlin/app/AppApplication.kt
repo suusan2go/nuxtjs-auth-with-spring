77f6666026ecb71b38220a946a641bca351eb997
suusan2go <ksuzuki180@gmail.com>
2017-11-26 23:30:17 +0900

    add authentication setting

--- spring-backend/src/main/kotlin/app/AppApplication.kt
+++ spring-backend/src/main/kotlin/app/AppApplication.kt
@@ -2,11 +2,23 @@ package app
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices
+import org.springframework.security.oauth2.provider.token.TokenStore
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore
 
 @SpringBootApplication
 @EnableAuthorizationServer
-class AppApplication
+class AppApplication: WebSecurityConfigurerAdapter(){
+    @Bean
+    fun tokenStore(): TokenStore = InMemoryTokenStore()
+
+    @Bean
+    fun tokenService(): ResourceServerTokenServices = DefaultTokenServices().also { it.setTokenStore(tokenStore()) }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(AppApplication::class.java, *args)
--- spring-backend/src/main/kotlin/app/grpc/interceptor/Oauth2AuthenticationInterceptor.kt
+++ spring-backend/src/main/kotlin/app/grpc/interceptor/Oauth2AuthenticationInterceptor.kt
@@ -9,8 +9,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
-import java.util.Objects;
-
 import com.google.common.base.Strings.nullToEmpty
 import io.grpc.ServerCallHandler
 import io.grpc.ServerCall
@@ -22,12 +20,12 @@ class Oauth2AuthenticationInterceptor(
 ): ServerInterceptor{
 
     override fun <ReqT, RespT> interceptCall(
-            call: ServerCall<ReqT, RespT>,
-            headers: Metadata,
-            next: ServerCallHandler<ReqT, RespT>): ServerCall.Listener<ReqT> {
-        val authHeader = nullToEmpty(headers.get(Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER)))
+            call: ServerCall<ReqT, RespT>?,
+            headers: Metadata?,
+            next: ServerCallHandler<ReqT, RespT>?): ServerCall.Listener<ReqT> {
+        val authHeader = nullToEmpty(headers!!.get(Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER)))
         if (!(authHeader.startsWith("Bearer ") || authHeader.startsWith("bearer "))) {
-            return next.startCall(call, headers)
+            return next!!.startCall(call, headers)
         }
 
         try {
@@ -49,13 +47,13 @@ class Oauth2AuthenticationInterceptor(
             throw Status.UNAUTHENTICATED.withDescription(e.message).withCause(e).asRuntimeException()
         }
 
-        return next.startCall(call, headers)
+        return next!!.startCall(call, headers)
     }
 
 
     private fun authenticationIsRequired(): Boolean {
         val existingAuth = SecurityContextHolder.getContext().authentication
-        if (Objects.isNull(existingAuth) || !existingAuth.isAuthenticated) {
+        if (existingAuth == null || !existingAuth.isAuthenticated) {
             return true
         }
 
